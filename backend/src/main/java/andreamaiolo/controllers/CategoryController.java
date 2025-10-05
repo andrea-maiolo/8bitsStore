0c4ebda23334c16a6427caba651d864bb467bf96
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-10-05 14:56:42 +0200

    Add CRUD operations for Category entity
    Introduced CategoryDto and extended CategoryController and CategoryService to support listing, updating, and deleting categories. These changes improve the API by enabling full CRUD functionality for categories.

--- backend/src/main/java/andreamaiolo/controllers/CategoryController.java
+++ backend/src/main/java/andreamaiolo/controllers/CategoryController.java
@@ -1,10 +1,14 @@
 package andreamaiolo.controllers;
 
+import andreamaiolo.dtos.CategoryDto;
 import andreamaiolo.dtos.CategoryPayload;
 import andreamaiolo.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/category")
@@ -12,10 +16,26 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public void saveNewCategory(@RequestBody CategoryPayload paylaod) {
         categoryService.saveCategory(paylaod);
+    }
+
+    @GetMapping
+    public List<CategoryDto> getAllCategories() {
+        return categoryService.findAll();
+    }
+
+    @DeleteMapping("/{categoryId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable UUID categoryId) {
+        this.categoryService.findAndDelete(categoryId);
+    }
+
+    @PatchMapping("/{categoryId}/update")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void updateCategory(@PathVariable UUID categoryId, @RequestBody CategoryPayload payload) {
+        this.categoryService.findAndUpdate(categoryId, payload);
     }
 }
--- backend/src/main/java/andreamaiolo/services/CategoryService.java
+++ backend/src/main/java/andreamaiolo/services/CategoryService.java
@@ -1,10 +1,16 @@
 package andreamaiolo.services;
 
+import andreamaiolo.dtos.CategoryDto;
 import andreamaiolo.dtos.CategoryPayload;
 import andreamaiolo.entities.Category;
+import andreamaiolo.exceptions.NotFoundException;
 import andreamaiolo.repositories.CategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -18,5 +24,29 @@ public class CategoryService {
         System.out.println(newCategory.getName());
         this.categoryRepo.save(newCategory);
         return newCategory;
+    }
+
+    public Category findById(UUID catId) {
+        return this.categoryRepo.findById(catId).orElseThrow(() -> new NotFoundException("category not found"));
+    }
+
+    public List<CategoryDto> findAll() {
+        List<Category> categoriesFound = this.categoryRepo.findAll();
+        List<CategoryDto> categoryDtoList = categoriesFound.stream()
+                .map(category -> new CategoryDto(category.getName()))
+                .collect(Collectors.toList());
+        return categoryDtoList;
+    }
+
+    public void findAndDelete(UUID categoryId) {
+        Category found = findById(categoryId);
+        this.categoryRepo.delete(found);
+    }
+
+    public void findAndUpdate(UUID categoryId, CategoryPayload payload) {
+        Category found = findById(categoryId);
+        found.setName(payload.name());
+        this.categoryRepo.save(found);
+
     }
 }
